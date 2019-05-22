2856905e5184208e8064e093380b34036305f61b
devin <zhangchuzhao@youmi.net>
2019-05-22 10:02:50 +0800

    doc: 增加LeetCode题解列表

---
+++ gen_readme_file.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+import os
+
+if __name__ == '__main__':
+    readme_template = 'README_TEMPLATE.md'
+    readme_file = 'README.md'
+    python_solution_dirpath = './docs/Leetcode_Solutions/Python'
+    file_path_template = '- [{filename}](./docs/Leetcode_Solutions/Python/{file})\n'
+    file_name_list = ''
+
+    for dirpath, dirnames, filenames in os.walk(python_solution_dirpath):
+        filenames.sort()
+        for filename in filenames:
+            file_name_list += file_path_template.format(filename=os.path.splitext(filename)[0], file=filename)
+        break
+
+    # print(file_name_list)
+
+    with open(readme_template, 'r') as fr, open(readme_file, 'w') as fw:
+        content = fr.read()
+        content = content.replace('{leetcode_solution_list}', file_name_list)
+        # print(content)
+        fw.write(content)
